1a116ab0daaf5e4304429c11272b731f106c98f5
Alexey Batyuta <alexey@batyuta.com>
2022-04-14 08:03:56 +0200

    Build procedure issue #16  * fixes the scm:add goal

---
+++ docs/spring-boot-server/testapidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.batyuta.challenge.lottoland.exception"},{"l":"com.batyuta.challenge.lottoland.test"}];updateSearchResults();
