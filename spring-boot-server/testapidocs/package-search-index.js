7f2fedcb20c279f97768c7204ebfe64bc5c35cac
Alexey Batyuta <alexey@batyuta.com>
2022-04-19 10:02:06 +0200

    [maven-scm-plugin] updates                                     0.45-SNAPSHOT GitHub Pages

--- spring-boot-server/testapidocs/package-search-index.js
+++ spring-boot-server/testapidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.batyuta.challenge.lottoland.exception"},{"l":"com.batyuta.challenge.lottoland.test"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.batyuta.challenge.lottoland"},{"l":"com.batyuta.challenge.lottoland.converter"},{"l":"com.batyuta.challenge.lottoland.exception"},{"l":"com.batyuta.challenge.lottoland.test"},{"l":"com.batyuta.challenge.lottoland.utils"},{"l":"com.batyuta.challenge.lottoland.web"}];updateSearchResults();
